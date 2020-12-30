ebe852dd4dd4aae276e42ca43f028d9e24b6bd23
Ulf Ivraeus <ulf.ivraeus@gmail.com>
2020-12-30 15:21:56 +0100

    Add GUI and logic for changing account password

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -1,13 +1,25 @@
-import { auth } from '../services/firebase';
+import { auth } from "../services/firebase";
 
 export function signup(email, password) {
-    return auth().createUserWithEmailAndPassword(email, password);
+  return auth().createUserWithEmailAndPassword(email, password);
 }
 
 export function login(email, password) {
-    return auth().signInWithEmailAndPassword(email, password);
+  return auth().signInWithEmailAndPassword(email, password);
 }
 
 export function logout() {
-    return auth().signOut();
+  return auth().signOut();
+}
+
+export function update(oldPassword, password) {
+  const currentUser = auth().currentUser;
+  const cred = auth.EmailAuthProvider.credential(
+    currentUser.email,
+    oldPassword
+  );
+
+  return currentUser
+    .reauthenticateWithCredential(cred)
+    .then(() => currentUser.updatePassword(password));
 }
