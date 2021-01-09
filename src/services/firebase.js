5083d7716691bd2838a2f6aa9c84696ab2accab2
Ulf Ivraeus <ulf.ivraeus@gmail.com>
2021-01-09 09:55:28 +0100

    Initialse more (all?) Firebase config fields
    Don't know how important this is but according to the
    Firebase docs, this is what one is supposed to do.
    (It worked well before though)

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,11 +1,15 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/database'
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
 
 const config = {
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DB_URL
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL
 };
 firebase.initializeApp(config);
 export const auth = firebase.auth;
