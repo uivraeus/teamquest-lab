b7bcfca6759bed5564568bc62ea6499712dbd789
ulf <ulf@hundenonallen.net>
2021-04-20 13:17:49 +0200

    Update contact e-mail address

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     <>
       <h1>Contact information</h1>
       <p>Questions and requests regarding this App should be sent by email to:</p>
-      <a href="mailto:uivraeus@gmail.com">uivraeus@gmail.com</a>
+      <a href="mailto:contact@teamquest.uivraeus.net?subject=Mini-TMQ">contact@teamquest.uivraeus.net</a>
     </>
   );
 };
