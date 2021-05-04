321f588d70ca0a8022f1b4fc7a36ec572b53ada3
ulf <ulf@hundenonallen.net>
2021-05-04 17:40:35 +0200

    Update Create according to new teams design
    Introduce a top-parent level state of all owned teams.
    This is propagated to all pages who depend on
    this information.
    
    The removal of the "owned teams" hook in each
    sub-page removes the annoying "loading state".
    
    Hopefully, this will also remove temporary state
    inconsistencies later along the road.

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,31 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SurveySetup from "../components/SurveySetup";
-import Teams from "../components/Teams";
-import useAppContext from "../hooks/AppContext";
+import RouteSelect from "../components/RouteSelect"
 import { useHistory } from "react-router-dom";
+import useAppContext from "../hooks/AppContext";
 
 import "./Create.css";
 
-const Create = () => {
-  const { user } = useAppContext();
-  const [selectedTeamId, setSelectedTeamId] = useState(null);
-
+//Parent must ensure that "teams" is always valid (not null/undefined)
+const Create = ({ teams }) => {
+  const { showAlert } = useAppContext();
+  const [selectedTeam, setSelectedTeam] = useState(null);
+  
   const history = useHistory();
   const surveyCreated = (surveyId) => {
     //console.log("Survey created: ", surveyId);
     history.push(`/creator/info/${surveyId}`);
   };
 
-  //Routing helpers shall ensure that we never end up here with user==null, but...
-  if (!user) return (<></>);
-
+  //If no team(s) defined, the user must create one via the Manage section
+  useEffect(() => {
+    if (teams.length === 0) {
+      history.push(`/creator/manage`);
+      showAlert("No team defined yet", 
+        "To initiate a survey you must first create a team. You have now been redirected to the Manage section where this can be done.");
+    }
+  }, [teams, history, showAlert]);
+  if (teams.length === 0) {
+    return null;
+  }
+  
   return (
     <div className="Create">
       <>
         <h1>Run a new survey</h1>
-        <Teams user={user} onSelected={setSelectedTeamId} />
-        {selectedTeamId ? (
-          <SurveySetup teamId={selectedTeamId} onCreated={surveyCreated} />
+        <label htmlFor="team-select">Team:</label>
+        <RouteSelect
+          options={teams}
+          textKey="alias"
+          elementId="team-select"
+          onSelected={setSelectedTeam}
+        />
+        {selectedTeam ? (
+          <SurveySetup teamId={selectedTeam.id} onCreated={surveyCreated} />
         ) : null}
       </>
     </div>
--- src/pages/Creator.js
+++ src/pages/Creator.js
@@ -1,5 +1,5 @@
 //3rd-party
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link, Redirect, Route, Switch, useHistory, useRouteMatch } from 'react-router-dom'
 
 //My sub-routes and stuff
@@ -11,15 +11,31 @@ import Manage from './Manage';
 import SurveyCatalog from './SurveyCatalog'; 
 import ChangePassword from './ChangePassword';
 import TerminateAccount from './TerminateAccount';
-
+import useOwnedTeams from '../hooks/OwnedTeams';
+import useAppContext from '../hooks/AppContext';
 import AppBtn from "../components/AppBtn";
+
 import { ReactComponent as SurveyNewIcon } from "../icons/survey-new.svg";
 import { ReactComponent as SurveyMonIcon } from "../icons/survey-monitor.svg";
 import { ReactComponent as SettingsIcon } from "../icons/settings.svg";
 
 import './Creator.css';
 
+/* Internal render-helper for pages that require an _initialized_ "teams" prop  */
+const teamsPage = (Component, teams, readError ) => {
+  if (readError) {
+    return () => (<p>Can't access user team configuration</p>)
+  }
+  else if (!teams) {
+    return () => (<p>Loading user team configuration...</p>);
+  } else {
+    return () => <Component teams={teams}/>
+  }
+};
+
 const Creator = () => {
+  const { teams, readError } = useOwnedTeams();
+  const {showAlert} = useAppContext();
   const { path } = useRouteMatch();
   const pathNew = `${path}/new`;
   const pathMonitor = `${path}/tracker`;
@@ -33,6 +49,15 @@ const Creator = () => {
   const goTo = (path) => {
     history.push(path);
   }
+
+  //Alert on db read error
+  useEffect( () => {
+    if (readError && showAlert) {
+      showAlert("Error reading user's team data", readError, "Error");
+      //Don't really know what to do in this case... something is wrong with
+      //the backend DB connection
+    }
+  }, [readError, showAlert])
 
   return (
     <>
@@ -68,13 +93,14 @@ const Creator = () => {
             </ul>    
           </div>
         </Route>
-        <Route path={pathNew} component={Create}></Route>
-        <Route exact path={pathMonitor} component={SurveyCatalog}></Route>
-        <Route path={`${pathMonitor}/:teamId`} component={SurveyCatalog}></Route>
+        <Route exact path={pathNew} render={ teamsPage(Create, teams, readError) } ></Route>
+        <Route path={`${pathNew}/:teamId`} render={ teamsPage(Create, teams, readError) } ></Route>
+        <Route exact path={pathMonitor} render={ teamsPage(SurveyCatalog, teams, readError) }></Route>
+        <Route path={`${pathMonitor}/:teamId`} render={ teamsPage(SurveyCatalog, teams, readError)}></Route>
         <Route path={`${path}/info/:surveyId`} component={SurveyInfo}></Route>
-        <Route path={`${pathInherit}/:transferId`} component={Inherit}></Route>
+        <Route path={`${pathInherit}/:transferId`} render={ teamsPage(Inherit, teams, readError)}></Route>
         <Route path={`${pathTransfer}/:transferId`} component={TransferInfo}></Route>
-        <Route exact path={pathManage} component={Manage}></Route>
+        <Route exact path={pathManage} render={ teamsPage(Manage, teams, readError) }></Route>
         <Route exact path={pathPassword} component={ChangePassword}></Route>
         <Route exact path={pathTerminate} component={TerminateAccount}></Route>
         <Redirect from={`${path}/`} to={`${path}/main`} />
