47eb4830d2ad35ed81170e6aeab213fa1d18e311
ulf <ulf@hundenonallen.net>
2021-05-15 16:05:30 +0200

    First attempt at lazy-loading
    I'm not sure about the Start page; for now it is lazy-loaded
    (as very few actual users visits it).
    
    Also, corrected bug if aborting a slow load of a md-page.
    (I have a feeling this was not the only "leak" of this kind...
    I'l have to review the rest of the code at some point)

--- src/pages/MarkdownPage.js
+++ src/pages/MarkdownPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import LoadingIndicator from "../components/LoadingIndicator";
 import MarkdownBlock from "../components/MarkdownBlock";
 import { useLocation } from 'react-router-dom';
 
@@ -14,18 +15,21 @@ const MarkdownPage = ({mdFileName}) => {
   //NOTE: this fix will fire at every "internal" navigation as well, even though the
   //browser's logic is sufficient in that case. But it it simpler to always run it
   //than to keep track of when it is really required.
-  const [loaded, setLoaded] = useState("");
+  const [loaded, setLoaded] = useState(null);
   const location = useLocation();
   useEffect(() => {
-    if (loaded && location.hash.startsWith("#")) {
+    if (loaded && loaded === mdFileName && location.hash.startsWith("#")) {
       const id = location.hash.slice(1);
       const element = document.getElementById(id);
       element && element.scrollIntoView();
     }
-  }, [location, loaded]);
+  }, [mdFileName, location, loaded]);
 
   return (
-    <MarkdownBlock mdFileName={mdFileName} onLoaded={setLoaded} />
+    <>
+      {!loaded ? <LoadingIndicator /> : null }
+      <MarkdownBlock mdFileName={mdFileName} onLoaded={setLoaded} />
+    </>
   );
 };
 
