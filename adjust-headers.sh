57aa4430c82331ca260e5514a4b185a057dfee09
ulf <uivraeus@gmail.com>
2021-09-30 17:28:06 +0200

    Revise solution of CSP headers
    The _headers file is now created from a template
    in combination with some environment variables

---
+++ adjust-headers.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# First fix the Firestore RTDB rules
+if [ -z "$CSP_RTDB_PATTERN" ]; then
+  >&2 echo Error: CSP_RTDB_PATTERN not defined.
+  exit 1
+else
+  #Assume no / in pattern (nothing to escape)
+  sed s/CSP_PLACEHOLDER_RTDB_PATTERN/$CSP_RTDB_PATTERN/g ./_headers_template > public/_headers
+fi
+
+# If Sentry is used, fix rules for that as well
+if [ -z "$REACT_APP_SENTRY_DSN" ]; then
+  echo Running without Sentry integration. Do not include corresponding CSP headers.
+  sed -i s/CSP_PLACEHOLDER_SENTRY_INGEST//g public/_headers
+else
+  if [ -z "$CSP_SENTRY_INGEST" ]; then
+    >&2 echo Error: CSP_SENTRY_INGEST not defined.
+    exit 1
+  else
+    ESCAPED=$(echo $CSP_SENTRY_INGEST | sed 's/\//\\\//g')
+    sed -i s/CSP_PLACEHOLDER_SENTRY_INGEST/$ESCAPED/g public/_headers
+  fi
+fi
+sed -i s/\|/' '/g public/_headers
